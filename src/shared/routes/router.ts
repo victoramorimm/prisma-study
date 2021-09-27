a086b7b57b17c3f44c5f90958806feb2d16a28c2
Victor Amorim <victorvmrgamer@gmail.com>
2021-09-27 19:54:32 -0300

    feat: add students route creation

--- src/shared/routes/router.ts
+++ src/shared/routes/router.ts
@@ -1,17 +1,34 @@
 import { Router } from "express";
+import CreateStudent from "../../modules/students/services/CreateStudent";
+import UsersRepository from "../../modules/users/repositories/prisma/UsersRepository";
 import CreateUser from "../../modules/users/services/CreateUser";
 
 const router = Router();
 
 router.post("/users", async function (req, res) {
   try {
-    const createUser = new CreateUser();
+    const usersRepository = new UsersRepository();
+    const createUser = new CreateUser(usersRepository);
 
     const { name, email } = req.body;
 
     const response = await createUser.run({ name, email });
 
     return res.status(201).json({ user: response });
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+router.post("/students/:userId", async function (req, res) {
+  try {
+    const createStudent = new CreateStudent();
+
+    const { userId } = req.params;
+
+    const response = await createStudent.run(parseInt(userId));
+
+    return res.status(201).json({ student: response });
   } catch (error: any) {
     return res.status(400).json({ error: error.message });
   }
