49bb7982606e8537c1fdcdd5f806028015e26774
Victor Amorim <victorvmrgamer@gmail.com>
2021-09-27 20:53:54 -0300

    feat: ensure repository is able to update a new user

--- src/modules/users/repositories/prisma/UsersRepository.spec.ts
+++ src/modules/users/repositories/prisma/UsersRepository.spec.ts
@@ -1,3 +1,4 @@
+import { prisma } from ".prisma/client";
 import { prismaMock } from "../../../../../singleton";
 import UsersRepository from "../prisma/UsersRepository";
 
@@ -12,10 +13,45 @@ describe("Users Repository", () => {
     prismaMock.user.create.mockResolvedValueOnce(user);
 
     const usersRepository = new UsersRepository();
+
     await expect(usersRepository.create(user)).resolves.toEqual({
       id: 1,
       name: "Victor",
       email: "teste@teste.com",
+    });
+  });
+
+  it("should be able to update a new user", async () => {
+    const dataToCreateUser = {
+      id: 1,
+      name: "Victor",
+      email: "teste@teste.com",
+    };
+
+    prismaMock.user.create.mockResolvedValueOnce(dataToCreateUser);
+
+    const usersRepository = new UsersRepository();
+
+    const createdUser = await usersRepository.create(dataToCreateUser);
+
+    const updatedMockUser = {
+      id: 1,
+      name: "Victor Atualizado",
+      email: "newEmail@teste.com",
+    };
+
+    prismaMock.user.update.mockResolvedValueOnce(updatedMockUser);
+
+    const updatedUser = await usersRepository.update({
+      name: "Victor Atualizado",
+      email: "newEmail@teste.com",
+      userId: createdUser.id,
+    });
+
+    expect(updatedUser).toEqual({
+      id: 1,
+      name: "Victor Atualizado",
+      email: "newEmail@teste.com",
     });
   });
 });
