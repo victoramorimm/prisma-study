95f5acc07a6977742af9a17deffd29e8c40935f4
Victor Amorim <victorvmrgamer@gmail.com>
2021-09-27 20:52:47 -0300

    feat: add update method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -9,9 +9,16 @@ export interface ICreateUsers {
   email: string;
 }
 
+export interface IDataToUpdate {
+  name: string;
+  email: string;
+  userId: number;
+}
+
 interface IUsersRepository {
   create({ name, email }: ICreateUsers): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
+  update({ name, email, userId }: IDataToUpdate): Promise<User>;
 }
 
 export default IUsersRepository;
